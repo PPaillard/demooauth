d20c079a78a0ed0b6fa49c0e1b2e09b4f655584d
PierreP <paillard.pierre@gmail.com>
2024-10-31 16:44:43 +0100

    project init

---
+++ src/main/java/fr/m2i/oauth2demo/controllers/TestController.java
@@ -0,0 +1,19 @@
+package fr.m2i.oauth2demo.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+}
